9bae968165e1ea706574ca4764910369282ab728
Tou Fong Lee <leetoufong@gmail.com>
2020-10-13 10:30:03 -0400

    Componentize todo items. Add completed styles.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import AddTodo from './components/AddTodo';
+import TodoItem from './components/TodoItem';
 import Button from './components/Button';
 import './App.scss';
 
@@ -43,7 +44,7 @@ export default function App() {
         setIsInvalid(false);
       }
 
-      newTodos.push({id: Date.now(), title: input.value, complete: false});
+      newTodos.push({id: Date.now(), title: input.value, completed: false});
       setTodos(newTodos)
       localStorage.setItem('todoList', JSON.stringify(newTodos))
       input.value = '';
@@ -101,13 +102,7 @@ export default function App() {
               </header>
               <ul className="list-unstyled">
                 {todos.filter(todo => !todo.completed).map((todo, index) => (
-                  <li key={index} className="TodoItem">
-                    <span className="TodoItem-title">{todo.title}</span>
-                    <nav className="TodoItem-nav">
-                      <Button onClick={() => handleTodoStatus(todo)} variant={`TodoItem`} title={`Complete todo`}><svg aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"/></svg></Button>
-                      <Button onClick={() => handleDeleteTodo(todo)} variant={`TodoItem`} title={`Delete todo`}><svg aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/><path d="M0 0h24v24H0z" fill="none"/></svg></Button>
-                    </nav>
-                  </li>
+                  <TodoItem key={index} todo={todo} handleTodoStatus={handleTodoStatus} handleDeleteTodo={handleDeleteTodo} />
                 ))}
               </ul>
             </>
@@ -124,13 +119,7 @@ export default function App() {
               </header>
               <ul className="list-unstyled">
                 {todos.filter(todo => todo.completed).map((todo, index) => (
-                  <li key={todo.id} className="TodoItem TodoItem--completed">
-                  <strike className="TodoItem-title">{todo.title}</strike>
-                  <nav className="TodoItem-nav">
-                    <Button onClick={() => handleTodoStatus(todo)} variant={`TodoItem`} title={`Undo completed todo`}><svg aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"/></svg></Button>
-                    <Button onClick={() => handleDeleteTodo(todo)} variant={`TodoItem`} title={`Delete todo`}><svg aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/><path d="M0 0h24v24H0z" fill="none"/></svg></Button>
-                  </nav>
-                </li>
+                  <TodoItem key={index} todo={todo} handleTodoStatus={handleTodoStatus} handleDeleteTodo={handleDeleteTodo} />
                 ))}
               </ul>
             </>
---
+++ src/components/TodoItem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Button from '../components/Button';
+import './TodoItem.scss';
+
+export default function TodoItem(props) {
+  const {todo, handleTodoStatus, handleDeleteTodo} = props;
+  
+  return (
+    <li className={`TodoItem ${todo.completed ? 'is-completed' : ''}`}>
+      {!todo.completed ? (
+        <span className="TodoItem-title">{todo.title}</span>
+      ) : (
+        <strike className="TodoItem-title">{todo.title}</strike>
+      )}
+      <nav className="TodoItem-nav">
+        <Button onClick={() => handleTodoStatus(todo)} variant={`TodoItem`} title={`Complete todo`}><svg aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"/></svg></Button>
+        <Button onClick={() => handleDeleteTodo(todo)} variant={`TodoItem`} title={`Delete todo`}><svg aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/><path d="M0 0h24v24H0z" fill="none"/></svg></Button>
+      </nav>
+    </li>
+  )
+}
