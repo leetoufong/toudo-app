a67570911e0a0123449ab7325c3ce3c8039f7e81
Lee, Tou Fong (CDC/DDNID/NCCDPHP/OD) (CTR) <pla9@cdc.gov>
2022-07-27 12:26:47 -0400

    Changing tab spaces

--- src/App.js
+++ src/App.js
@@ -5,127 +5,127 @@ import Button from './components/Button';
 import './App.scss';
 
 export default function App() {
-  const [todos, setTodos] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isInvalid, setIsInvalid] = useState(false);
+	const [todos, setTodos] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [isInvalid, setIsInvalid] = useState(false);
 
-  useEffect(() => {
-    setIsLoading(true);
+	useEffect(() => {
+		setIsLoading(true);
 
-    const fetchData = async () => {
-      const data = await localStorage.getItem('todoList');
+		const fetchData = async () => {
+			const data = await localStorage.getItem('todoList');
 
-      if (!data) {
-        //if data doesn't exist, create a object and set it on localStorage
-        localStorage.setItem('todoList', JSON.stringify([]))
-      } else {
-        //data stored returns as string, parse it to become readable
-        const newTodos = JSON.parse(data);
-        //set our state 
-        setTodos(newTodos);
-      }
+			if (!data) {
+				//if data doesn't exist, create a object and set it on localStorage
+				localStorage.setItem('todoList', JSON.stringify([]))
+			} else {
+				//data stored returns as string, parse it to become readable
+				const newTodos = JSON.parse(data);
+				//set our state 
+				setTodos(newTodos);
+			}
 
-      //turn off loader
-      setIsLoading(false);
-    }
-    
-    fetchData();
-  }, [])
+			//turn off loader
+			setIsLoading(false);
+		}
 
-  function handleAddTodo(event) {
-    event.preventDefault();
-    const input = event.target.querySelector('input');
-    const newTodos = [...todos];
+		fetchData();
+	}, [])
 
-    //validate text input
-    if (input.value.length > 0) {
-      if (isInvalid) {
-        input.classList.remove('is-invalid');
-        setIsInvalid(false);
-      }
+	function handleAddTodo(event) {
+		event.preventDefault();
+		const input = event.target.querySelector('input');
+		const newTodos = [...todos];
 
-      newTodos.push({id: Date.now(), title: input.value, completed: false});
-      setTodos(newTodos)
-      localStorage.setItem('todoList', JSON.stringify(newTodos))
-      input.value = '';
-    } else {
-      input.classList.add('is-invalid');
-      setIsInvalid(true);
-    }
-  }
+		//validate text input
+		if (input.value.length > 0) {
+			if (isInvalid) {
+				input.classList.remove('is-invalid');
+				setIsInvalid(false);
+			}
 
-  function handleTodoStatus(item) {
-    const newTodos = [...todos];
-    
-    if (!item.completed) {
-      item.completed = true;
-    } else {
-      item.completed = false;
-    }
+			newTodos.push({ id: Date.now(), title: input.value, completed: false });
+			setTodos(newTodos)
+			localStorage.setItem('todoList', JSON.stringify(newTodos))
+			input.value = '';
+		} else {
+			input.classList.add('is-invalid');
+			setIsInvalid(true);
+		}
+	}
 
-    setTodos(newTodos);
-    localStorage.setItem('todoList', JSON.stringify(newTodos));
-  }
+	function handleTodoStatus(item) {
+		const newTodos = [...todos];
 
-  function handleDeleteTodo(item) {
-    const newTodos = [...todos];
+		if (!item.completed) {
+			item.completed = true;
+		} else {
+			item.completed = false;
+		}
 
-    newTodos.forEach((todo, i) => {
-      if (todo.id === item.id) {
-        newTodos.splice(i, 1);
-      }
-    })
+		setTodos(newTodos);
+		localStorage.setItem('todoList', JSON.stringify(newTodos));
+	}
 
-    setTodos(newTodos);
-    localStorage.setItem('todoList', JSON.stringify(newTodos))
-  }
+	function handleDeleteTodo(item) {
+		const newTodos = [...todos];
 
-  return (
-    <div className="App">
-      {isLoading ? (
-        <h2 className="App-subtitle">Todo App</h2>
-      ) : (
-        <>
-          <header className="App-header">
-            <h1 className="App-title">Todo App</h1>
-            <AddTodo isInvalid={isInvalid} handleAddTodo={handleAddTodo} />
-          </header>
+		newTodos.forEach((todo, i) => {
+			if (todo.id === item.id) {
+				newTodos.splice(i, 1);
+			}
+		})
 
-          {/* Current Todos */}
-          {todos.filter((todo) => !todo.completed).length < 1 ? (
-            <p>No todo items. You must not be busy.</p>
-          ) : (
-            <>
-              <header className="App-subheader">
-                <h2 className="App-subtitle">Current Todos:</h2>
-                <span>{todos.filter(todo => !todo.completed).length} Item(s)</span>
-              </header>
-              <ul className="list-unstyled">
-                {todos.filter(todo => !todo.completed).map((todo, index) => (
-                  <TodoItem key={index} todo={todo} handleTodoStatus={handleTodoStatus} handleDeleteTodo={handleDeleteTodo} />
-                ))}
-              </ul>
-            </>
-          )}
-          
-          {/* Completed Todos */}
-          {todos.filter((todo) => todo.completed).length < 1 ? (
-            ''
-          ) : (
-            <>
-              <header className="App-subheader">
-                <h2 className="App-subtitle">Completed Todos:</h2>
-                <span>{todos.filter(todo => todo.completed).length} Item(s)</span>
-              </header>
-              <ul className="list-unstyled">
-                {todos.filter(todo => todo.completed).map((todo, index) => (
-                  <TodoItem key={index} todo={todo} handleTodoStatus={handleTodoStatus} handleDeleteTodo={handleDeleteTodo} />
-                ))}
-              </ul>
-            </>
-          )}
-        </>
-      )}
-    </div>
-  )
+		setTodos(newTodos);
+		localStorage.setItem('todoList', JSON.stringify(newTodos))
+	}
+
+	return (
+		<div className="App">
+			{isLoading ? (
+				<h2 className="App-subtitle">Todo App</h2>
+			) : (
+				<>
+					<header className="App-header">
+						<h1 className="App-title">Todo App</h1>
+						<AddTodo isInvalid={isInvalid} handleAddTodo={handleAddTodo} />
+					</header>
+
+					{/* Current Todos */}
+					{todos.filter((todo) => !todo.completed).length < 1 ? (
+						<p>No todo items. You must not be busy.</p>
+					) : (
+						<>
+							<header className="App-subheader">
+								<h2 className="App-subtitle">Current Todos:</h2>
+								<span>{todos.filter(todo => !todo.completed).length} Item(s)</span>
+							</header>
+							<ul className="list-unstyled">
+								{todos.filter(todo => !todo.completed).map((todo, index) => (
+									<TodoItem key={index} todo={todo} handleTodoStatus={handleTodoStatus} handleDeleteTodo={handleDeleteTodo} />
+								))}
+							</ul>
+						</>
+					)}
+
+					{/* Completed Todos */}
+					{todos.filter((todo) => todo.completed).length < 1 ? (
+						''
+					) : (
+						<>
+							<header className="App-subheader">
+								<h2 className="App-subtitle">Completed Todos:</h2>
+								<span>{todos.filter(todo => todo.completed).length} Item(s)</span>
+							</header>
+							<ul className="list-unstyled">
+								{todos.filter(todo => todo.completed).map((todo, index) => (
+									<TodoItem key={index} todo={todo} handleTodoStatus={handleTodoStatus} handleDeleteTodo={handleDeleteTodo} />
+								))}
+							</ul>
+						</>
+					)}
+				</>
+			)}
+		</div>
+	)
 }
